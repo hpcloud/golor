2b0d155550786a2e30ea16e5765b570904da4027
Sridhar Ratnakumar <sridharr@activestate.com>
2013-06-26 15:37:10 -0700

    move unique color assigner to golor package

--- golor.go
+++ golor.go
@@ -2,6 +2,7 @@
 package golor
 
 import (
+	"crypto/sha1"
 	"fmt"
 )
 
@@ -28,6 +29,25 @@ func Colorize(s string, fg int, bg int) string {
 // must range from 0 to 5.
 func RGB(red, green, blue int) int {
 	return 16 + (red * 36) + (green * 6) + blue
+}
+
+// Assign and return an unique color for this string. Automatically
+// avoid black/white colors.
+func AssignColor(s string) int {
+	maxColor := 211 // prevent whitish colors; use prime number for mod.
+	minColor := 17  // avoid 16 colors, including the black.
+	fg := minColor + stringId(s, maxColor-minColor+1)
+	return fg
+}
+
+func stringId(s string, mod int) int {
+	h := sha1.New()
+	h.Write([]byte(s))
+	sum := 0
+	for _, n := range h.Sum(nil) {
+		sum += int(n)
+	}
+	return sum % mod
 }
 
 func init() {
