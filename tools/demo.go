20f411da4011a56996066e3a39950fc5cfaa198a
Sridhar Ratnakumar <sridharr@activestate.com>
2013-10-16 00:06:54 -0700

    add background color printing to demo

--- tools/demo.go
+++ tools/demo.go
@@ -25,6 +25,6 @@ func main() {
 	time.Sleep(time.Second)
 	for i := 255; i >= 0; i-- {
 		fmt.Println(golor.Colorize("Foreground", i, -1))
-		// fmt.Println(golor.ColorizeBg("Background", i, 100))
+		fmt.Println(golor.Colorize("Background", -1, i))
 	}
 }
